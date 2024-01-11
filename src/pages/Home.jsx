0bcd72394c840d081bb400fed1e73b3d3692d4bc
Anjil <niraulaanjil@gmail.com>
2024-01-11 21:35:01 +0545

    color changed and slogan added.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -66,7 +66,7 @@ const Home = () => {
         {/* Predict Button (Visible only after uploading) */}
         {audioFile && (
           <div
-            className="bg-green-500 hover:bg-green-600 px-6 py-3 mt-4 rounded-full text-white cursor-pointer transition-all duration-300"
+            className="bg-rose-500 hover:bg-rose-600 px-6 py-3 mt-4 rounded-full text-white cursor-pointer transition-all duration-300"
             onClick={handlePredict}
           >
             Predict
@@ -75,8 +75,8 @@ const Home = () => {
 
         {/* Display Predicted Emotion */}
         {predictedEmotion && (
-          <div className="mt-4 text-2xl text-rose-500 font-bold">
-            Predicted Emotion: {predictedEmotion}
+          <div className="mt-8 text-2xl text-rose-500 font-normal">
+            Decode the Feels: Emotion Unveiled - {predictedEmotion}
           </div>
         )}
       </div>
